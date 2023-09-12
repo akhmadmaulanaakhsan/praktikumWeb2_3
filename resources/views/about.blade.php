a11a488a59d2f66f9b6137d612198aa2e2adeb33
akhmadmaulanaakhsan <maulanaakhsan10@gmail.com>
2023-09-12 19:45:59 +0700

    controller dan migration

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -7,7 +7,7 @@
     <title>About</title>
 </head>
 <body>
-    <h1>Halaman About</h1>
+    <h1>Ini Halaman About</h1>
     <h1>{{$name}}</h1>
     <h1>{{$email}}</h1>
 <script src="js/script.js"></script>
